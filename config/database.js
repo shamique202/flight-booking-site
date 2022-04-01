ecb52bd12a1c056d0cd8ba56393f4d302ff27537
shamique202 <shamique.e@gmail.com>
2022-04-01 00:15:28 -0400

    added destinations

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies', {
+mongoose.connect('mongodb://localhost/flights', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+// shortcut to the mongoose object
+const db = mongoose.connection;
+
+db.on('connected', function () {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
--- models/flights.js
+++ models/flights.js
@@ -1,3 +1,34 @@
-//basic database for schema (mongoose)
+// basic database for schema (mongoose)
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// models for destinations
+const destinationSchema = new mongoose.Schema({
+    airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+    },
+    arrival: Date
+
+});
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest', 'United']
+    },
+    flightNo: {
+        Type: Number,
+        Required: true,
+        min: 10,
+        max: 9999
+    },
+
+
+
+
+
+});
+
+
+
+
+module.exports = mongoose.model('Flight', flightsSchema);
