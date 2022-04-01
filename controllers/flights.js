0b4f1b88ee5305d146bf8308ab567a321f4ffd45
shamique202 <shamique.e@gmail.com>
2022-04-01 01:06:38 -0400

    flight controllers

---
+++ controllers/flights.js
@@ -0,0 +1,29 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlight,
+    create
+}
+
+function index(req, res) {
+    Flight.find({}, function (err, flights) {
+        res.render('flights', { flights });
+    });
+};
+
+function newFlight(req, res) {
+    //respond with a form for entering a new flight
+    res.render('flights/new');
+    res.redirect('/flights');
+};
+
+function create(req, res) {
+    var flight = new Flight(req.body);
+    flight.save(function (err) {
+        // one way to handle errors
+        if (err) return res.render('flights/new');
+        // for now, redirect right back to new.ejs
+        res.redirect('/flights');
+    });
+};
