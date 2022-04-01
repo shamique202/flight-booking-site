3991f930997736479ca4b16e2c60c43143abdcec
shamique202 <shamique.e@gmail.com>
2022-04-01 00:41:13 -0400

    new flight routes

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', flightsCtrl.index);
 
+// GET /flights/new
+router.get('/new', flightsCtrl.new);
+// POST /flights
+router.post('/', flightsCtrl.create);
 
-module.exports = router;
