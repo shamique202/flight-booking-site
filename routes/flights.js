51ff5fd05db781bd7bf5bd0edfd9f931d316a411
shamique202 <shamique.e@gmail.com>
2022-04-02 02:12:31 -0400

    flight comments

--- routes/flights.js
+++ routes/flights.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
-/* GET /flights list */
+/* GET /flights lists */
 router.get('/', flightsCtrl.index);
-/* GET (fill out the form) */
+/* GET (fill out the form information) */
 router.get('/new', flightsCtrl.new);
-/* GET - lists flight information */
+/* GET - lists incoming flight information */
 router.get('/:id', flightsCtrl.show);
 /* POST new flight listings */
 router.post('/', flightsCtrl.create);
