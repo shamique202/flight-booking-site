7b9d069b0f1efef6454307ed4670dd3a8df325a5
shamique202 <shamique.e@gmail.com>
2022-04-04 23:08:58 -0400

    updates

--- routes/arrivals.js
+++ routes/arrivals.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-const arrivalsCtrl = require('../controllers/arrivals');
+const express = require("express");
+const router = express.Router();
+const arrivalsCtrl = require("../controllers/arrivals");
 
-router.post('/flights/:id/arrivals', arrivalsCtrl.create)
+router.post("/flights/:id/arrivals", arrivalsCtrl.create);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,47 @@
-const createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-const cookieParser = require('cookie-parser');
-let logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
 
-require('./config/database')
-
-const indexRouter = require('./routes/index');
-const flightsRouter = require('./routes/flights');
-// arrivals route
-const arrivalsRouter = require('./routes/arrivals')
+var indexRouter = require("./routes/index");
+var flightsRouter = require("./routes/flights");
+var arrivalsRouter = require("./routes/arrivals");
+var ticketsRouter = require("./routes/tickets");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', arrivalsRouter);
-app.use('/', indexRouter);
-app.use('/flights', flightsRouter);
-// catch 404 and forwards to error handler
+app.use("/", indexRouter);
+app.use("/flights", flightsRouter);
+app.use("/", arrivalsRouter);
+app.use("/", ticketsRouter);
+
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
